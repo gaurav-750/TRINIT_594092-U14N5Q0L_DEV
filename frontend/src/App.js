32c258f820739f61986d3d0c6ede44b9d9c46244
KedarKK1 <officialkedark1@gmail.com>
2023-02-11 19:51:50 +0530

    seach NGO page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import Home from './pages/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import NGO from './pages/Ngo_Dashboard/NGO';
+import NGO_Profile from './pages/NGO_Profile/NGO_Profile';
 
 function App() {
   return (
     <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/ngo" element={<NGO />} />
+        <Route path="/ngo/:pg_no" element={<NGO />} />
+        <Route path="/ngo/profile/:pg_no" element={<NGO_Profile />} />
     </Routes>
   );
 }
---
+++ frontend/src/pages/NGO_Profile/NGO_Profile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NGO_Profile = () => {
+  return (
+    <div>NGO_Profile</div>
+  )
+}
+
+export default NGO_Profile
