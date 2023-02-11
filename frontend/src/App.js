7a5fa773e3e243e7c7fdb181b578cfa45bbb4e78
KedarKK1 <officialkedark1@gmail.com>
2023-02-11 18:24:06 +0530

    routing added front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import NGO from './pages/Ngo_Dashboard/NGO';
+
 function App() {
   return (
-    <div className="App">
-                                                                                                
-      <Home></Home>
-    </div>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ngo" element={<NGO />} />
+    </Routes>
   );
 }
 
